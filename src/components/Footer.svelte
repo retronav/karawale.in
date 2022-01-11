3eb991f2cacf68c83e2175d1162e058ea059d33a
Pranav Karawale <pdkarawale31@gmail.com>
2022-01-11 22:30:46 +0530

    Trivial fixes to prevent overflow

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,12 +6,16 @@
   <p>Social Media links</p>
   <ul class="links">
     {#each siteConfig.menus.footerSocial as { label, path }, _}
-    <li class="link"><a href={path}>{label}</a></li>
+      <li class="link"><a href={path}>{label}</a></li>
     {/each}
   </ul>
   <p>All opinions posted here are my own, unless specified otherwise.</p>
-  <small>
-  <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />The content on this website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</small>
+  <small
+    >The content on this website is licensed under a
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"
+      >CC BY-NC 4.0 License</a
+    >.</small
+  >
 </footer>
 
 <style lang="scss">
@@ -19,6 +23,7 @@
   footer {
     @include v.accent();
     text-align: center;
+    margin-top: 1rem;
     padding: 1rem 2rem;
     ul.links {
       @include v.links();
