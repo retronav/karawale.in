113cb583ab7d4da0909c30262531e23fbd900844
Pranav Karawale <pdkarawale31@gmail.com>
2022-01-12 22:41:26 +0530

    Make navbar and footer look sane

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,19 +3,19 @@
 </script>
 
 <footer>
-  <p>Social Media links</p>
-  <ul class="links">
-    {#each siteConfig.menus.footerSocial as { label, path }, _}
-      <li class="link"><a href={path}>{label}</a></li>
+  <section class="links">
+    <p>Social Media links</p>
+    {#each siteConfig.menus.footerSocial as { label, path }, i}
+      <a href={path}>{label}</a>
+      {#if (i < siteConfig.menus.footerSocial.length - 1)}
+        <span>|&nbsp;</span>
+      {/if}
     {/each}
-  </ul>
-  <p>All opinions posted here are my own, unless specified otherwise.</p>
-  <small
-    >The content on this website is licensed under a
-    <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"
-      >CC BY-NC 4.0 License</a
-    >.</small
-  >
+  </section>
+  &copy; {new Date().getUTCFullYear()} {siteConfig.copyrightHolder}
+  <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">
+    CC BY-NC 4.0
+  </a>
 </footer>
 
 <style lang="scss">
@@ -23,18 +23,17 @@
   footer {
     @include v.accent();
     text-align: center;
+    font-size: 0.9rem;
     margin-top: 1rem;
     padding: 1rem 2rem;
-    ul.links {
+    section.links {
       @include v.size-lg {
         width: 70%;
         margin: 0 auto;
+        margin-bottom: 1rem;
       }
-      @include v.links();
-      grid-auto-rows: 3rem;
-      li, li a {
-        width: 100%;
-        height: 100%;
+      p {
+        margin-top: 0;
       }
     }
   }
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,31 +5,34 @@
 <header>
   <nav class="navbar">
     <a class="title" href="/">{siteConfig.title}</a>
-
-    <ul class="links">
-      {#each siteConfig.menus.navbar as { label, path }, _}
-        <li class="link"><a href={path}>{label}</a></li>
+    <section class="links">
+      {#each siteConfig.menus.navbar as { label, path }, i}
+        <a href={path}>{label}</a>
+        {#if i < siteConfig.menus.navbar.length - 1}
+          <span>|&nbsp;</span>
+        {/if}
       {/each}
-    </ul>
+    </section>
   </nav>
 </header>
 
 <style lang="scss">
   @use '../styles/variables.scss' as v;
   nav.navbar {
+    @include v.accent();
     display: grid;
     grid-column-gap: 1rem;
     justify-content: space-between;
     align-items: center;
     padding: 1rem;
     @include v.size-lg {
-      grid-template-columns: repeat(3, 1fr);
-      height: 3rem;
+      grid-template-columns: repeat(2, 1fr);
+      height: 2rem;
     }
     @include v.size-md {
       grid-template-columns: 1fr;
-      grid-template-rows: repeat(2, 3rem);
-      min-height: 3rem;
+      grid-template-rows: repeat(2, 1fr);
+      min-height: 2rem;
     }
 
     a.title {
@@ -39,20 +42,8 @@
         grid-row: 1 / 2;
       }
     }
-    ul.links {
-      @include v.size-lg {
-        grid-column: 3 / 4;
-      }
-      @include v.size-md {
-        grid-row: 2 / 3;
-        li a {
-          // Make the links tap size large enough to be accessible
-          height: 3rem;
-          display: grid;
-          align-items: center;
-        }
-      }
-      @include v.links();
+    section.links {
+      text-align: right;
     }
   }
 </style>
