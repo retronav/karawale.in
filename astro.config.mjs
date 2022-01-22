4af8045a20ca82ca42e9da92d8316db979b1ce19
Pranav Karawale <pdkarawale31@gmail.com>
2022-01-22 15:20:17 +0530

    Make sitemap to include "Read more" link

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: 'http://obnerd.in', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: 'https://obnerd.in', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
--- src/site.config.ts
+++ src/site.config.ts
@@ -2,6 +2,7 @@ interface Config {
   copyrightHolder: string;
   title: string;
   description: string;
+  site: string;
   menus: {
     [key: string]: {
       label: string;
@@ -19,6 +20,7 @@ const config: Partial<Config> = {
   copyrightHolder: 'Pranav Karawale',
   title: 'Pranav Karawale',
   description: "Pranav Karawale's personal website",
+  site: 'https://obnerd.in',
   menus: {
     navbar: [
       {
