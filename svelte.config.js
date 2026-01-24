40f93788d48f626e9405045a7ea290357dd44f25
Pranav <52596591+retronav@users.noreply.github.com>
2026-01-24 14:40:11 +0000

    Use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
